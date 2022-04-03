46efb5cba91ca1ff6d0bec48b09adf75ef450f02
Callum <1903181@uad.ac.uk>
2022-04-03 19:43:19 +0100

    2 players controlling 4 characters improved
    full turn order works
    doesn't work after a pawn dies

--- MbR-eSports-Multiplayer-main/batch.bat
+++ MbR-eSports-Multiplayer-main/batch.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\Callum\Desktop\CMP208\stormy-night-games\MbR-eSports-Multiplayer-main\MbR_eSports\MbR_eSports.uproject" -game -ResX=960 -ResY=540 -WinX=0 -WinY=20 –log
+"C:\Program Files\Epic Games\UE_4.26\Engine\Binaries\Win64\UE4Editor.exe" "C:\Users\Callum\Desktop\stormy-night-games-joao\stormy-night-games-joao\MbR-eSports-Multiplayer-main\MbR_eSports\MbR_eSports.uproject" -game -ResX=960 -ResY=540 -WinX=0 -WinY=20 –log
